5b6d146a7fe8dd2db2e75a390833d5d095af5dd0
/Foufoudu34 <76502393+Foufou-exe@users.noreply.github.com>
2023-03-27 19:01:59 +0200

    Version : 0.1.0 Dev (#4)
    * 💢Modification
    
    * 📦 NEW: TEST CODE
    
    * RENAME
    
    * 🐛 FIX: PROBLEME NOMMAGE
    
    * 🐛 FIX: REMOVE LIB
    
    * 🐛 FIX: READAPTATION
    
    * Ouai
    
    * 🐛 FIX: workfolw
    
    * Update and rename Yspeed.py to yspeed.py
    
    * 🐛 FIX: test
    
    * 🐛 FIX:SPACE
    
    * 📦 NEW:  Perfect Files
    
    * 📦 NEW: NICE
    
    * 📦 NEW: package
    
    * 📦 NEW: TEST UNITAIRE
    
    * 🐛 FIX: RESOLV LIB
    
    * MODIF
    
    * 🐛 FIX: Correction
    
    * 🐛 FIX: Provle
    
    * 🐛 FIX: Correction
    
    * 📦 NEW: Files
    
    * TETS
    
    * Verife
    
    * 🐛 FIX: Problème
    
    * 🐛 FIX: TEST
    
    * PY
    
    * 🐛 FIX: TEST
    
    * 🐛 FIX:  PROBLEME CHEMIN
    
    * TEST 2
    
    * TEST
    
    * TEST3
    
    * 🐛 FIX: ESSAIE
    
    * 📦 NEW: TEST
    
    * 🐛 FIX: RENAME
    
    * 🐛 FIX: solutuion
    
    * 📦 NEW: Ajout de code
    
    * 🐛 FIX:  Probleme yspeed
    
    * 📦 NEW: IMAGES
    
    * Delete .github/images directory
    
    * 🐛 FIX: COMPATIBILITE
    
    * 🐛 FIX: test
    
    * 📦 NEW: PHOTO
    
    * 📦 NEW:  LOGO
    
    * 🐛 FIX: TXT
    
    * 🐛 FIX: RENAME
    
    * 📦 NEW: NEW
    
    * 🐛 FIX: travis
    
    * ‼️ BREAKING: TRAVIS
    
    * 🐛 FIX: travis
    
    * 🐛 FIX: TRAVIS
    
    * 🐛 FIX: TRAVIS
    
    * 🐛 FIX: test
    
    * 🐛 FIX: yspeed
    
    * 🐛 FIX:  Display
    
    * 📦 NEW:  FILES
    
    * 🚀 RELEASE: 0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# Yspeed
+![logo](https://socialify.git.ci/Foufou-exe/Yspeed/image?description=1&descriptionEditable=Yspeed%20is%20a%20library%20that%20scrapes%20the%20Speedtest%20site&font=Jost&forks=1&issues=1&logo=https%3A%2F%2Fraw.githubusercontent.com%2FFoufou-exe%2FYspeed%2Fdev%2F.github%2Fimages%2Foffice.svg&name=1&owner=1&pulls=1&stargazers=1&theme=Dark)
+
+YSpeed is a Python library that scrapes the Speedtest site using Selenium and displays the results with Rich. This library makes it easy to retrieve internet connection speed data (upload, download and latency) in an automated manner and display it elegantly using the Rich library.
+
+## Features
+
+- Speedtest.net site scraping with Selenium
+- Retrieve internet connection speed results (upload, download and latency)
+- Display results with Rich for better readability
+
+## Installation
+
+Make sure you have Python 3.6 or later installed. To install YSpeed, use the following command:
+
+```python
+pip install yspeed
+```
+
+or :
+
+Clone the Project
+
+```cmd
+git clone https://github.com/Foufou-exe/Yspeed
+```
+
+Go to the directory
+
+```cmd
+cd Yspeed
+```
+
+Install the dependencies
+
+```cmd
+pip install -r requirements.txt
+```
+
+Run the Yspeed.py script
+
+```cmd
+python yspeed.py
+```
+
+## Usage
+
+Here is an example of how to use the YSpeed library:
+
+```python
+from yspeed import YSpeed
+
+ys = YSpeed()
+result = ys.run_speedtest()
+ys.display_results(result)
+```
+
+or use script yspeed.py
+
+```bash
+python yspeed.py
+```
+
+## Dependencies
+
+YSpeed depends on the following libraries:
+
+- selenium
+- rich
+
+Make sure you also have a Selenium-compatible driver installed for your preferred browser (Chrome, Firefox, etc.).
+
+## Contribution
+
+Feel free to contribute to this project by opening issues or submitting pull requests. All contributions are welcome.
+
+## License
+
+YSpeed is distributed under the MIT license. See the ``LICENSE`` file for more information.
---
+++ test_yspeed.py
@@ -0,0 +1,123 @@
+"""
+Test file for the yspeed.py file
+"""
+import unittest
+from unittest.mock import Mock, patch, MagicMock
+from selenium.webdriver.common.by import By
+from contextlib import contextmanager
+from yspeed import Yspeed, gather_network_info, print_network_info
+
+@contextmanager
+def progress_context_manager():
+    """
+    Context manager for the progress bar
+    """
+    progress_mock = MagicMock()
+    progress_mock.__enter__.return_value = progress_mock
+    yield progress_mock
+    progress_mock.__exit__.assert_called_once()
+class TestYourClass(unittest.TestCase):
+    """ 
+    Test class for the Yspeed class
+    """
+    
+    @patch('yspeed.Halo')
+    @patch('yspeed.time.sleep', MagicMock(return_value=None))
+    def test_run_speedtest(self, mock_halo):
+        """ Test the run_speedtest method"""
+        # Replace 'YourClass' with the actual name of the class containing the `run_speedtest` function
+        speedtest_obj = Yspeed()
+
+        # Mock the get_speedtest method to return a predefined dictionary
+        mock_speedtest = {'download': '100 Mbps', 'upload': '50 Mbps', 'ping': '20 ms'}
+        speedtest_obj.get_speedtest = MagicMock(return_value=mock_speedtest)
+
+        # Call the run_speedtest function and get the results
+        result = speedtest_obj.run_speedtest()
+
+        # Assert that the result is as expected
+        self.assertEqual(result, mock_speedtest)
+
+        # Assert that the Halo spinner is called with the expected parameters
+        mock_halo.assert_called_with(spinner={'interval': 100, 'frames': ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏']}, text="Démarrage du Speedtest", color="red", text_color="yellow")
+
+    
+    def setUp(self):
+        """ 
+        Setup the test class
+        """
+        self.yspeed = Yspeed()
+
+
+    def test_get_ip_info(self):
+        """
+        Test the get_ip_info method
+        """
+        with patch('requests.get') as mock_get:
+            mock_get.return_value.json.return_value = {
+                'ip': '1.2.3.4',
+                'city': 'Test City',
+                'region': 'Test Region',
+                'country': 'Test Country',
+                'org': 'Test Operator'
+            }
+            result = self.yspeed.get_ip_info()
+            self.assertIsInstance(result, dict)
+            self.assertEqual(result["ip"], "1.2.3.4")
+            self.assertEqual(result["city"], "Test City")
+            self.assertEqual(result["region"], "Test Region")
+            self.assertEqual(result["country"], "Test Country")
+            self.assertEqual(result["operator"], "Test Operator")
+            
+    def test_gather_network_info(self):
+        """
+        Test the gather_network_info method
+        """
+        # Mock Yspeed to return test data
+        speedtest_mock = Mock()
+        speedtest_mock.get_ip_info.return_value = {"ip": "1.2.3.4"}
+        speedtest_mock.best_serveur.return_value = {"Serveur": "Test Server"}
+        speedtest_mock.get_speedtest.return_value = {"download": "100 Mbps"}
+
+        with progress_context_manager() as progress_mock:
+            result = gather_network_info(speedtest_mock, progress_mock)
+
+        self.assertIn("ip", result)
+        self.assertIn("Serveur", result)
+        self.assertIn("download", result)
+
+    def test_print_network_info(self):
+        """
+        Test the print_network_info method
+        """
+        # Mock Console to capture printed output
+        console_mock = Mock()
+
+        info = {
+            "operator": "Test Operator",
+            "ip": "1.2.3.4",
+            "city": "Test City",
+            "region": "Test Region",
+            "country": "Test Country",
+            "fournisseur": "Test Fournisseur",
+            "Serveur": "Test Server",
+            "download": "100 Mbps",
+            "upload": "50 Mbps",
+            "ping": "10 ms"
+        }
+
+        print_network_info(console_mock, info)
+
+        console_mock.print.assert_any_call("Operator: [bold green]Test Operator[/bold green]", style="blue", justify="center")
+        console_mock.print.assert_any_call("IP: [bold green]1.2.3.4[/bold green]", style="blue", justify="center")
+        console_mock.print.assert_any_call("Server: [bold green]Test Server[/bold green]", style="blue", justify="center")
+        console_mock.print.assert_any_call("Download: [bold green]100 Mbps[/bold green]", style="blue", justify="center")
+        console_mock.print.assert_any_call("Upload: [bold green]50 Mbps[/bold green]", style="blue", justify="center")
+        console_mock.print.assert_any_call("Ping: [bold green]10 ms[/bold green]", style="blue", justify="center")
+        console_mock.print.assert_any_call("City: [bold green]Test City[/bold green]", style="blue", justify="center")
+        console_mock.print.assert_any_call("Region: [bold green]Test Region[/bold green]", style="blue", justify="center")
+        console_mock.print.assert_any_call("Country: [bold green]Test Country[/bold green]", style="blue", justify="center")
+        console_mock.print.assert_any_call("Fournisseur: [bold green]Test Fournisseur[/bold green]", style="blue", justify="center")
+
+if __name__ == '__main__':
+    unittest.main()
